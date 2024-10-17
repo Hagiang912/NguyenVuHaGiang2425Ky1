e45e1e27f68a9c7af6df69e22ade09cc11792865
unknown <it3.cntt.humg@gmail.com>
2024-10-17 20:16:37 +0700

    Create_Project

---
+++ NguyenVuHaGiang044.NetMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenVuHaGiang044.NetMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
